ebf44c28803cd25fa3509a2fc59091eaf0340eb0
corytishii <cory_ishii93@att.net>
2021-10-11 12:55:02 -0700

    edited index.html & app.js

--- ishii.cory/js/app.js
+++ ishii.cory/js/app.js
@@ -2,45 +2,46 @@
 // DOCUMENT READY
 $(()=>{
 
+   checkUserId();
 
-	checkUserId();
-
-	// Event DeLegation
-	$(document)
+   // Event Delegation
+   $(document)
 
 
-	// Form SUBMITS
-	.on("submit", "signin-form",function(e) {
-		e.preventDefault();
-
-		checkSigninForm();
-	})
-	.on("submit","#list-add-form",function(e) {
+   // FORM SUBMITS
+   .on("submit","#signin-form",function(e) {
+      e.preventDefault();
+      checkSigninForm();
+   })
+   .on("submit","#list-add-form",function(e) {
       e.preventDefault();
    })
 
-	// ANCHOR CLICKS
-	.on("click",".js-logout",function(e){
-		e.preventDefault();
-		sessionStorage.removeItem("userId");
-		checkUserId();
-	})
 
-	.on("click","[data-activate]",function(e){
-		e.preventDefault();
-		let target = $(this).data("activate");
-		$(target).addClass("active");		
-	})
-	.on("click","[data-deactivate]",function(e){
-		e.preventDefault();
-		let target = $(this).data("deactivate");
-		$(target).removeClass("active");		
-	})
-	.on("click","[data-toggle]",function(e){
-		e.preventDefault();
-		let target = $(this).data("toggle");
-		$(target).toggleClass("active");		
-	})
+   // ANCHOR CLICKS
+   .on("click",".js-logout",function(e) {
+      e.preventDefault();
+      sessionStorage.removeItem("userId");
+      checkUserId();
+   })
+
+
+
+   .on("click","[data-activate]",function(e){
+      e.preventDefault();
+      let target = $(this).data("activate");
+      $(target).addClass("active");
+   })
+   .on("click","[data-deactivate]",function(e){
+      e.preventDefault();
+      let target = $(this).data("deactivate");
+      $(target).removeClass("active");
+   })
+   .on("click","[data-toggle]",function(e){
+      e.preventDefault();
+      let target = $(this).data("toggle");
+      $(target).toggleClass("active");
+   })
 
 
 
